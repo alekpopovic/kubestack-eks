4aaa28e86b1d1a2d7c49843aa6c1526379cdf74c
Aleksandar Popovic <aleksandar.popovic@linux.com>
2023-06-07 01:58:40 +0200

    set ingres

--- eks_gc0_eu-central-1_service_nginx.tf
+++ eks_gc0_eu-central-1_service_nginx.tf
@@ -11,3 +11,18 @@ module "eks_gc0_eu-central-1_service_nginx" {
     ops  = {}
   }
 }
+
+module "eks_gc0_eu-west-1_dns_zone" {
+  providers = {
+    aws        = aws.eks_gc0_eu-central-1
+    kubernetes = kubernetes.eks_gc0_eu-central-1
+  }
+
+  # make sure to match your cluster module's version
+  source = "github.com/kbst/terraform-kubestack//aws/cluster/elb-dns?ref=v0.18.1-beta.0"
+
+  ingress_service_name      = "ingress-nginx-controller"
+  ingress_service_namespace = "ingress-nginx"
+
+  metadata_fqdn = module.eks_gc0_eu-west-1.current_metadata["fqdn"]
+}
